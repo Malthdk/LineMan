40fe92873d73e9161343a49de2b20c538df579b0
Malte Hølledig <malteholledig@Maltes-MacBook-Pro.local>
2017-09-21 22:02:14 +0200

    made jump sound and cleaned up some code in Player

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -8,7 +8,6 @@ public class Player : MonoBehaviour {
 	public float maxJumoHeightPainting;
 	public float minJumpHeight = 1f;				//Minimum JumpHeight
 	[HideInInspector]
-	public float purpMinJumpVelocity = -9.3f;		//Minimum jump velocity when purple - have to be defined here cause of mathematical equation
 	public float timeToJumpApex = .65f;				//Time to reach highest point (4875 was before)
 	public float accelerationTimeAirborn = .2f;		//Acceleration while airborne
 	public float accelerationTimeGrounded = .5f;	//Acceleration while grounded
@@ -20,17 +19,6 @@ public class Player : MonoBehaviour {
 	public Vector2 input;
 	[HideInInspector]
 	public int wallDirX;
-
-	//FOR WALLSLIDING
-	[HideInInspector]
-	public float wallSlideSpeedMax = 0.01f;		//Maximum wall spide speed
-	private float wallStickTime = 1f;		//Time before releasing from wall when input is away from wall
-	float timeToWallUnstick;
-	[HideInInspector]
-	public bool wallSliding;
-	[HideInInspector]
-	public float wjXSmoothing, wjYSmoothing;
-	private float wjAcceleration = 0.05f;
 
 	public float gravity;					//gramaxJumpVelocity to player
 	[HideInInspector]
@@ -44,36 +32,14 @@ public class Player : MonoBehaviour {
 
 	private Animator animator;		//ANIMATION
 	[HideInInspector]
-//	public bool doubleJumped, tripleJumped, hasTripleJumped = false, hasDoubleJumped = false;
-//	private float doubleJumpVelocity;
-//	public float doubleJumpReduction;
-//	private float tripleJumpVelocity;
-//	public float tripleJumpReduction;
 	public float gravityModifierFall;
 	public float gravityModifierJump;
-//	public float gravityModifierPaintingJump;
-//	public float gravityModifierPaintingFall;
 
 	// Has player landed?
 	private bool landed = true;
 
 	public float ghostJumpingBuffer = 0.15f;
 	private float timeSinceJump;
-
-	// FOR SOUND
-	public AudioClip jumpSoundTakeOff;
-	public AudioClip jumpSoundLanding;
-	public AudioClip paintingSound;
-	public AudioClip paintingSplashSound;
-
-	[HideInInspector]
-	public AudioSource paintingSoundSource;
-	[HideInInspector]
-	public AudioSource jumpSoundSource;
-
-	private float volLowRange = .6f;
-	private float volHighRange = .8f;
-	private float volLanding;
 
 	//ParticleSystems for dJump and tJump
 	private ParticleSystem doubleJumpParticle;
@@ -90,6 +56,8 @@ public class Player : MonoBehaviour {
 	[HideInInspector]
 	public static Player _instance;
 
+	private float pitchValue;
+
 	public static Player instance {	// Makes it possible to call script easily from other scripts
 		get {
 			if (_instance == null) {
@@ -102,7 +70,6 @@ public class Player : MonoBehaviour {
 	void Start () 
 	{
 		movementUnlocked = true;
-//		jumpSoundSource = gameObject.transform.GetChild(9).GetComponent<AudioSource>();
 		controller = GetComponent<Controller2D>();
 		intoLine = GetComponent<IntoLine>();
 		animator = transform.GetComponentInChildren<Animator>();		//ANIMATION
@@ -151,11 +118,6 @@ public class Player : MonoBehaviour {
 		animator.SetBool("ground", controller.collisions.below);	//ANIMATION
 		animator.SetBool("transforming", intoLine.transforming);
 		animator.SetFloat("vSpeed", velocity.y);					//ANIMATION
-		//animator.SetBool("OnWall", wallSliding);
-		//animator.SetBool("Dashing", abilities.isDashing);
-		//animator.SetBool ("Soaring", abilities.soaring);
-//		animator.SetBool("DoubleJump", doubleJumped);
-//		animator.SetBool("TripleJump", tripleJumped);
 			
 		if (!controller.collisions.below) {
 			timeSinceJump += Time.deltaTime;
@@ -168,41 +130,18 @@ public class Player : MonoBehaviour {
 			if (controller.collisions.below)
 			{
 				FirstJump();
+				AkSoundEngine.SetRTPCValue ("Pitch", 0);
 				AkSoundEngine.PostEvent ("Jump", gameObject);
 			}
-
-//			if(!controller.collisions.below)
-//			{
-//				/*This is where double jump is handled*/
-//				if (!hasDoubleJumped)
-//				{	
-//					if (timeSinceJump < ghostJumpingBuffer){	
-//						FirstJump ();
-//					} else {
-//						DoubleJump ();
-//					}
-//
-//				}
-//				else if (!hasTripleJumped && hasDoubleJumped)
-//				{
-//					TripleJump ();
-//				}
-//			}
 		}
 
 		if (!controller.collisions.below)
 		{
 			landed = false;
 			moveSpeed = airSpeed;
-			volLanding = (velocity.y / -30);
 		}
 		else if (controller.collisions.below && landed == false)
 		{
-//			jumpSoundSource.PlayOneShot(jumpSoundLanding, volLanding);
-//			hasTripleJumped = false;
-//			hasDoubleJumped = false;
-//			tripleJumped = false; //animation
-//			doubleJumped = false; //animation
 			moveSpeed = groundSpeed;
 			landed = true;
 		}
@@ -213,19 +152,17 @@ public class Player : MonoBehaviour {
 		else 
 		{
 			timeToJumpApex = gravityModifierJump;//0.92f;	//72
-//			doubleJumpVelocity = maxJumpVelocity/doubleJumpReduction;
-//			tripleJumpVelocity = maxJumpVelocity/tripleJumpReduction;
 		}
 
 		if (Input.GetButtonUp("Jump"))					//For variable jump
 		{
-			if (velocity.y > minJumpVelocity) //&& (!hasDoubleJumped && !hasTripleJumped) to get it on DJ and TJ
+			if (velocity.y > minJumpVelocity) 
 			{
 				velocity.y = minJumpVelocity;									//When space is released set velocity y to minimum jump velocity
+				AkSoundEngine.SetRTPCValue ("Pitch", 15);
 			}
 		}
-
-		if (movementUnlocked == true)
+	if (movementUnlocked == true)
 		{
 			velocity.y += gravity * Time.deltaTime;	
 			controller.Move(velocity * Time.deltaTime, input);				//Moving character
@@ -238,43 +175,9 @@ public class Player : MonoBehaviour {
 		}
 	}
 
-	void FirstJump() {
-		float vol = Random.Range(volLowRange, volHighRange);
-//		jumpSoundSource.PlayOneShot(jumpSoundTakeOff, vol);
+	void FirstJump() 
+	{
 		velocity.y = maxJumpVelocity;
 //		animator.SetBool("Ground", false);
 	}
-
-//	void DoubleJump() {
-//		float vol = Random.Range(volLowRange, volHighRange);
-//		if (timeSinceJump < ghostJumpingBuffer) {
-//			jumpSoundSource.PlayOneShot(jumpSoundTakeOff, vol);
-//			velocity.y = maxJumpVelocity;
-////			animator.SetBool("Ground", false);
-//		}
-//		jumpSoundSource.PlayOneShot(jumpSoundTakeOff, (.1f + vol));
-//		velocity.y = 0;
-//		Debug.Log ("double jumped");
-//		doubleJumped = true; //This causes an animation bugg where we jump from a wall and then doublejump is is set to true so we canno transition into jump animation.
-//		hasDoubleJumped = true;
-//		//abilities.secondJump = true;
-//		velocity.y = doubleJumpVelocity;
-////		doubleJumpParticle.Play();
-//	}
-
-//	void TripleJump() {
-//		float vol = Random.Range(volLowRange, volHighRange);
-//		jumpSoundSource.PlayOneShot(jumpSoundTakeOff, (.2f + vol));
-//		velocity.y = 0;
-//		Debug.Log ("tripple jumped");
-//		tripleJumped = true; //animation
-//		hasTripleJumped = true; 
-//		velocity.y = tripleJumpVelocity;
-////		tripleJumpParticle.Play();
-//	}
-
-	//IEnumerator SpecialJump(float velocity)
-	//{
-	//	yield return new WaitForEndOfFrame();
-	//}
 }
