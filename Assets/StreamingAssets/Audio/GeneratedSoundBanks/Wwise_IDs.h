2843aeb1877debf1fb65451da154ddcde5d6e4dc
Malte Hølledig <malteholledig@Maltes-MacBook-Pro.local>
2017-10-22 12:07:40 +0200

    Fixed wwise version so we are able to build

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,19 +11,46 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID BACKGROUND = 3650723969U;
+        static const AkUniqueID COLLECT = 2756333705U;
+        static const AkUniqueID CURRENT = 2037989808U;
+        static const AkUniqueID DIE = 445985469U;
+        static const AkUniqueID ENDLEVEL = 1054659462U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID TRANSITION = 1865857008U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace NEW_STATE_GROUP
+        {
+            static const AkUniqueID GROUP = 2012657067U;
+
+            namespace STATE
+            {
+            } // namespace STATE
+        } // namespace NEW_STATE_GROUP
+
+    } // namespace STATES
+
     namespace GAME_PARAMETERS
     {
-        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
-        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
-        static const AkUniqueID SS_AIR_FURY = 1029930033U;
-        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
-        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
-        static const AkUniqueID SS_AIR_RPM = 822163944U;
-        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
-        static const AkUniqueID SS_AIR_STORM = 3715662592U;
-        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
-        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+        static const AkUniqueID PITCH = 1908158473U;
     } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BACKGROUND = 3650723969U;
+        static const AkUniqueID COLLECT = 2756333705U;
+        static const AkUniqueID CURRENT = 2037989808U;
+        static const AkUniqueID DIE = 445985469U;
+        static const AkUniqueID ENDLEVEL = 1054659462U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID TRANSITION = 1865857008U;
+    } // namespace BANKS
 
     namespace BUSSES
     {
