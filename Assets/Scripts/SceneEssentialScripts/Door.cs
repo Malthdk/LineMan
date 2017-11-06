11adb1f4a7910fda3bf0449b9cc5800dbf9adb87
Gwanplants <cnelson.private@gmail.com>
2017-11-06 20:53:46 +0100

    made new -end goal- in all levels

--- Assets/Scripts/SceneEssentialScripts/Door.cs
+++ Assets/Scripts/SceneEssentialScripts/Door.cs
@@ -14,13 +14,19 @@ public class Door : MonoBehaviour {
 	private Door cDoor;
 	public bool open, closed;
 	private TextMesh myTextMesh;
+	private ParticleSystem.MainModule mainSystem;
+	private SpriteRenderer sRendererEnd;
+	private SpriteRenderer sRendererMid;
 	private BoxCollider2D myCollider;
 	public ParticleSystem pSystem;
 
 	void Start () 
 	{
+		sRendererEnd = transform.GetChild(0).GetComponent<SpriteRenderer>();
+		sRendererMid = transform.GetChild(1).GetComponent<SpriteRenderer>();
+		mainSystem = transform.GetChild(3).GetComponent<ParticleSystem>().main;
 		myTextMesh = transform.GetComponentInChildren<TextMesh>();
-		pSystem = transform.GetComponentInChildren<ParticleSystem>();
+		pSystem = transform.GetChild(4).GetComponent<ParticleSystem>();
 
 		myCollider = GetComponent<BoxCollider2D>();
 		cDoor = (GameObject.FindWithTag("cPoint") == null)?null:GameObject.FindWithTag("cPoint").GetComponent<Door>();
@@ -28,16 +34,22 @@ public class Door : MonoBehaviour {
 		if (cDoor != null && cDoor.gameObject != this.gameObject)
 		{
 			midPoint = true;
+			sRendererMid.enabled = true;
+			sRendererEnd.enabled = false;
 			open = true;
 		}
 		else if (cDoor != null && cDoor.gameObject == this.gameObject)
 		{
 			endPoint = true;
+			sRendererMid.enabled = false;
+			sRendererEnd.enabled = true;
 			closed = true;
 		}
 		else if (cDoor == null)
 		{
 			endPoint = true;
+			sRendererMid.enabled = false;
+			sRendererEnd.enabled = true;
 			open = true;
 		}
 	}
@@ -46,11 +58,17 @@ public class Door : MonoBehaviour {
 	{
 		if (open)
 		{
-			myTextMesh.color = openColor;
+			//myTextMesh.color = openColor;
+			mainSystem.startColor = openColor;
+			sRendererEnd.color = openColor;
+			sRendererMid.color = openColor;
 		}
 		else if (closed)
 		{
-			myTextMesh.color = closedColor;
+			//myTextMesh.color = closedColor;
+			mainSystem.startColor = closedColor;
+			sRendererEnd.color = closedColor;
+			sRendererMid.color = closedColor;
 		}
 	}
 	void OnTriggerEnter2D(Collider2D other)
@@ -65,6 +83,7 @@ public class Door : MonoBehaviour {
 			else if (midPoint && open)
 			{
 				myCollider.enabled = false;
+				sRendererMid.enabled = false;
 				cDoor.open = true;
 				pSystem.Play();
 			}
