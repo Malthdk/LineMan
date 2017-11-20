79271ad867aef1172ecf58a57560bedd0a6a3231
Gwanplants <cnelson.private@gmail.com>
2017-11-20 16:29:39 +0100

    started on particle effect

--- Assets/Scripts/PrefabScripts/Tools/PlatformDrawer.cs
+++ Assets/Scripts/PrefabScripts/Tools/PlatformDrawer.cs
@@ -4,35 +4,58 @@ using UnityEngine;
 
 public class PlatformDrawer : MonoBehaviour {
 
-	public Transform otherTrans;
+	public List<Transform> otherTrans;
 
 	public float position;
-
 	public float minScale = 1.175789f;
 	public float maxScale = 5.675f;
 	public float minPlatPos = 48.19f;
 	public float maxPlatPos = 58.19f;
 
+	public bool scaleX, scaleY;
+
 	void Start () 
 	{
-		position = transform.position.x;
+		CalcPosition();
 	}
 	
 
 	void Update () 
 	{
-		position = transform.position.x;
+		CalcPosition();
 		HandleSize();
 	}
 
 	void HandleSize()
 	{
-		otherTrans.localScale = new Vector3(0.437499f, Map(position, minPlatPos, maxPlatPos, minScale, maxScale), 1f);
+		for (int i = 0; i < otherTrans.Count; i++)
+		{
+			if (scaleX)
+			{
+				otherTrans[i].localScale = new Vector3(Map(position, minPlatPos, maxPlatPos, minScale, maxScale), 0f, 1f);	
+			}
+			else if (scaleY)
+			{
+				otherTrans[i].localScale = new Vector3(0.437499f, Map(position, minPlatPos, maxPlatPos, minScale, maxScale), 1f);
+			}
+		}	
 	}
 
 	public float Map(float value, float inMin, float inMax, float outMin, float outMax)
 	{
 		return(value-inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+	}
+
+	public void CalcPosition()
+	{
+		if (scaleX)
+		{
+			position = transform.position.x;	
+		}
+		else if (scaleY)
+		{
+			position = transform.position.y;
+		}
 	}
 
 }
